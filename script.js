3d98e84b5fbdfe82d94192b14c206fcde191ac41
Joseph Liyon PT <120583161+jliyon23@users.noreply.github.com>
2024-01-24 03:10:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,250 @@
+let products = {
+    data: [
+      {
+        productName: "HP Victus Gaming Laptop 12th Gen Intel Core i5-12450H 15-fa0092TX",
+        category: "hp",
+        price: "67,990",
+        info: "i5-12450H \n16GB RAM \n512GB SSD \nNVIDIA GeForce RTX 3050 4GB graphics \n144Hz",
+        link : "https://www.amazon.in/HP-i5-12450H-GeForce-graphics-15-fa0092TX/dp/B0BZYNJZWD/ref=sr_1_6?crid=3M4K46XD8Z2QJ&keywords=hp+victus&qid=1706018105&sprefix=hp+vic%2Caps%2C215&sr=8-6",
+        image: "./img/hpvictusi511th.jpg",
+      },
+      {
+        productName: "HP Victus AMD Ryzen 7 Octa Core 5800H 15-fb0137AX",
+        category: "hp",
+        price: "67,990",
+        info: "Ryzen 7 5800H \n16GB RAM \n512GB SSD \nNVIDIA GeForce RTX 3050 4GB graphics \n144Hz",
+        link : "https://www.flipkart.com/hp-victus-amd-ryzen-7-octa-core-5800h-16-gb-512-gb-ssd-windows-11-home-4-graphics-nvidia-geforce-rtx-3050-15-fb0137ax-gaming-laptop/p/itmc0108d5fa7687?pid=COMGZUGT3NMUDWFS&lid=LSTCOMGZUGT3NMUDWFSSGDZZI&marketplace=FLIPKART&q=hp+victus&store=4rr%2Ftz1&srno=s_1_4&otracker=search&otracker1=search&fm=organic&iid=d900b5de-6cfa-4315-8db2-d8a4b64780d8.COMGZUGT3NMUDWFS.SEARCH&ppt=hp&ppn=homepage&ssid=uqm2ddh6k00000001706018875010&qH=8fa28d763aa5c003",
+        image: "./img/hpvictusi511th.jpg",
+      },
+      {
+        productName: "HP Victus AMD Ryzen 5 Hexa Core 5600H 15-fb0135AX",
+        category: "hp",
+        price: "57,990",
+        info: "Ryzen 5 5600H \n8GB RAM \n512GB SSD \nNVIDIA GeForce RTX 3050 4GB graphics",
+        link : "https://www.flipkart.com/hp-victus-amd-ryzen-5-hexa-core-5600h-8-gb-512-gb-ssd-windows-11-home-4-graphics-nvidia-geforce-rtx-3050-15-fb0135ax-gaming-laptop/p/itm740ddb5e8081e?pid=COMGZUGTYNZFXNGG&lid=LSTCOMGZUGTYNZFXNGGHOWGKH&marketplace=FLIPKART&q=hp+victus&store=4rr%2Ftz1&spotlightTagId=TrendingId_4rr%2Ftz1&srno=s_1_6&otracker=search&otracker1=search&fm=Search&iid=d900b5de-6cfa-4315-8db2-d8a4b64780d8.COMGZUGTYNZFXNGG.SEARCH&ppt=sp&ppn=sp&qH=8fa28d763aa5c003",
+        image: "./img/hpvictusi511th.jpg",
+      },
+      {
+        productName: "HP 2023 Intel Core i5 12th Gen 1235U 15s-fy5002TU",
+        category: "hp",
+        price: "49,990",
+        info: "i5-1235U 12th Gen \n8GB RAM \n512GB SSD \nIntel Iris Xe Graphics",
+        link : "https://www.amazon.in/HP-i5-1235U-Anti-Glare-Graphics-15s-fq5202TU/dp/B0CJM5Z6FY/ref=sr_1_10?crid=1KP6U4129C98R&keywords=hp%2Bpavilion%2B15%2Bi5%2B12th%2Bgen&qid=1706021204&sprefix=hp%2Bpavilion%2B15%2Bi5%2B1th%2Bgen%2Caps%2C238&sr=8-10&th=1",
+        image: "./img/hppvi512th.jpg",
+      },
+      {
+        productName: "HP Laptop 15s, AMD Ryzen 7 5700U ey2001AU",
+        category: "hp",
+        price: "53,990",
+        info: "Ryzen 7 5700U \n16GB RAM \n512GB SSD \nAMD Radeon graphics",
+        link : "https://www.amazon.in/HP-15-6-inch-Graphics-Speakers-ey2001AU/dp/B0C9DDN2JB/ref=sr_1_2_sspa?crid=1KTP02HZXT18W&keywords=hp+victus+ryzen+7&qid=1706018863&sprefix=hp+victus+ryzen+%2Caps%2C226&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+        image: "./img/hppvr7.jpg",
+      },
+      {
+        productName: "HP Pavilion 14, AMD Ryzen 5 5625U ec1005AU",
+        category: "hp",
+        price: "56,199",
+        info: "Ryzen 5 5625U \n16GB RAM \n512GB SSD \nAMD Radeon graphics",
+        link : "https://www.amazon.in/HP-Pavilion-Micro-Edge-Graphics-14-ec1005AU/dp/B0BS1BVRZH/ref=sr_1_6?crid=Y2Z3PZA3H0HZ&keywords=hp+ryzen+5&qid=1706020639&sprefix=hp+ryzen+%2Caps%2C220&sr=8-6",
+        image: "./img/hppvr5.jpg",
+      },
+      {
+        productName: "HP Laptop 15, 13th Gen Intel Core i5-1335U",
+        category: "hp",
+        price: "59,990",
+        info: "i5-1335U 13th Gen \n8GB RAM \n512GB SSD \nIntel Iris Xe Graphics",
+        link : "https://www.amazon.in/HP-i5-12450H-GeForce-graphics-15-fa0092TX/dp/B0BZYNJZWD/ref=sr_1_6?crid=3M4K46XD8Z2QJ&keywords=hp+victus&qid=1706018105&sprefix=hp+vic%2Caps%2C215&sr=8-6",
+        image: "./img/hppvi513th.webp",
+      },
+      {
+        productName: "ASUS Vivobook Pro 15 OLED, AMD Ryzen 5 5600H M3500QC-L1502WS",
+        category: "asus",
+        price: "67,990",
+        info: "Ryzen 5 5600H \n16GB RAM \n512GB SSD \nNVIDIA RTX 3050 4GB",
+        link : "https://www.amazon.in/HP-i5-12450H-GeForce-graphics-15-fa0092TX/dp/B0BZYNJZWD/ref=sr_1_6?crid=3M4K46XD8Z2QJ&keywords=hp+victus&qid=1706018105&sprefix=hp+vic%2Caps%2C215&sr=8-6",
+        image: "./img/asusvb15oled.jpg",
+      },
+      {
+        productName: "ASUS Vivobook 15, Intel Core i7-12650H 12th Gen X1502ZA-EJ741WS",
+        category: "asus",
+        price: "59,990",
+        info: "i7-12650H 12th Gen \n16GB RAM \n512GB SSD \nIntel Iris Xe Graphics",
+        link : "https://www.amazon.in/HP-i5-12450H-GeForce-graphics-15-fa0092TX/dp/B0BZYNJZWD/ref=sr_1_6?crid=3M4K46XD8Z2QJ&keywords=hp+victus&qid=1706018105&sprefix=hp+vic%2Caps%2C215&sr=8-6",
+        image: "./img/asusi712th.jpg",
+      },
+      {
+        productName: "ASUS TUF Gaming F15, Intel Core i5-11400H 11th Gen FX506HC-HN089W",
+        category: "asus",
+        price: "57,990",
+        info: "i5-11400H \n8GB RAM \n512GB SSD \nNVIDIA RTX 3050 4GB",
+        link : "https://www.amazon.in/ASUS-i5-11400H-RTX-3050-Graphics-FX506HC-HN089W/dp/B0C7QWHQK6/ref=sr_1_7?crid=1OE2VQJJKWWFA&keywords=asus+tuf&qid=1706021792&sprefix=asus+tuf%2Caps%2C204&sr=8-7",
+        image: "./img/asustuf.jpg",
+      },
+      {
+        productName: "Lenovo IdeaPad Slim 3 Intel Core i7 12th Gen 82RK011EIN",
+        category: "lenovo",
+        price: "62,990",
+        info: "i7-1255U  \n16GB RAM \n512GB SSD \nIntel Iris Xe Graphics",
+        link : "https://www.amazon.in/Lenovo-IdeaPad-39-62cm-Windows-82RK011EIN/dp/B0CMGZCDRW/ref=sr_1_1_sspa?crid=3IXTT7I7SJ0QO&keywords=lenovo%2Blaptops%2Bi7&qid=1706078053&sprefix=lenovo%2Blaptops%2Caps%2C217&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1",
+        image: "./img/lenovoi712th.jpg",
+      },
+      {
+        productName: "Lenovo IdeaPad Gaming 3 AMD Ryzen 5 6600H 82SB00V3IN",
+        category: "lenovo",
+        price: "62,990",
+        info: "Ryzen 5 6600H \n16GB RAM \n512GB SSD \nNVIDIA RTX 3050 4GB",
+        link : "https://www.amazon.in/Lenovo-IdeaPad-39-62cm-Keyboard-82SB00V3IN/dp/B0CGV49DLW/ref=sr_1_6?crid=2FC7XS0V0EMG8&keywords=lenovo+gaming+laptop&qid=1706091236&sprefix=lenovo+gamin%2Caps%2C225&sr=8-6",
+        image: "./img/lenovoideapad.jpg",
+      },
+      {
+        productName: "MSI GF63 Thin, Intel Core i5-11260H 11UC-1474IN",
+        category: "msi",
+        price: "54,990",
+        info: "Ryzen 5 6600H \n16GB RAM \n512GB SSD \nNVIDIA RTX 3050 4GB",
+        link : "https://www.amazon.in/MSI-i5-11260H-Windows-GeForce-11UC-1474IN/dp/B0C6F4TM71/ref=sr_1_2_sspa?crid=242F79N5BMLFF&keywords=msi%2Bgaming%2Blaptop&qid=1706091497&sprefix=msi%2B%2Caps%2C216&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1",
+        image: "./img/msi54k.jpg",
+      },
+      {
+        productName: "MSI Thin GF63, Intel 12th Gen. i5-12450H A12VE-071IN",
+        category: "msi",
+        price: "62,990",
+        info: "i5-12450H \n16GB RAM \n512GB SSD \nNVIDIA RTX 4050 6GB GDDR6",
+        link : "https://www.amazon.in/MSI-i5-12450H-Windows-GeForce-A12VE-071IN/dp/B0BZNT33D6/ref=sr_1_10?crid=242F79N5BMLFF&keywords=msi%2Bgaming%2Blaptop&qid=1706091497&sprefix=msi%2B%2Caps%2C216&sr=8-10&th=1",
+        image: "./img/msi70k.jpg",
+      },
+      {
+        productName: "MSI Modern 14, Intel 12th Gen. i7-1255U C12M-459IN",
+        category: "msi",
+        price: "49,990",
+        info: "i7-1255U \n16GB RAM \n512GB SSD \nIntel Iris Xe Graphics",
+        link : "https://www.amazon.in/MSI-i7-1255U-Windows-Graphics-C12M-459IN/dp/B09YCX5F52/ref=sr_1_1_sspa?crid=OC5QXJWVDN2N&keywords=msi+laptop&qid=1706091841&sprefix=msi+laptop%2Caps%2C221&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+        image: "./img/msi50k.jpg",
+      },
+      {
+        productName: "Acer Nitro 5 Gaming Laptop AMD Ryzen™ 7 7735HS Octa-Core Processor",
+        category: "acer",
+        price: "69,990",
+        info: "Ryzen 7 7735HS 8 cores \n16GB RAM \n512GB SSD \nNVIDIA RTX 3050 4GB",
+        link : "https://www.amazon.in/Acer-Octa-Core-Processor-Graphics-AN515-47/dp/B0C1BGCPX9/ref=sr_1_1_sspa?crid=1EFAD5XS29GBC&keywords=acer+nitro&qid=1706092053&sprefix=acer+ni%2Caps%2C293&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+        image: "./img/acernitro.jpg",
+      },
+      {
+        productName: "Acer Aspire 5 Gaming Laptop Intel Core i5 12th gen ",
+        category: "acer",
+        price: "52,990",
+        info: "i5-12500H \n16GB RAM \n512GB SSD \nNVIDIA RTX 2050 4GB",
+        link : "https://www.amazon.in/MSI-i7-1255U-Windows-Graphics-C12M-459IN/dp/B09YCX5F52/ref=sr_1_1_sspa?crid=OC5QXJWVDN2N&keywords=msi+laptop&qid=1706091841&sprefix=msi+laptop%2Caps%2C221&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+        image: "./img/aceraspireg.jpg",
+      },
+      {
+        productName: "Acer Aspire 5 13th Gen Intel Core i5 A515-58M",
+        category: "acer",
+        price: "52,490",
+        info: "i5-1335U \n16GB RAM \n512GB SSD \nIntel Iris Xe Graphics",
+        link : "https://www.amazon.in/Acer-Aspire-Windows-A515-58M-Display/dp/B0C5D32X14/ref=sr_1_4?crid=2OS6C546DG49U&keywords=acer+laptop+i5+13th+generation&qid=1706092670&sprefix=acer+laptop+i5+13%2Caps%2C212&sr=8-4",
+        image: "./img/aceraspire.jpg",
+      },
+    ],
+  };
+  //
+  for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //price
+    let price = document.createElement("h6");
+    price.classList.add("price");
+    price.innerText = "₹" + i.price;
+    container.appendChild(price);
+    //info
+    let info = document.createElement("h6");
+    info.innerText = i.info;
+    container.appendChild(info);
+
+    let buy = document.createElement("a");
+    buy.setAttribute("href", i.link);
+    buy.target = "_blank";
+    buy.innerText = "Buy Now";
+    let buybtn = document.createElement("button");
+    buybtn.appendChild(buy);
+    buybtn.classList.add("button-29");
+    container.appendChild(buybtn);
+  
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+  
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+  
+  //Initially display all products
+  window.onload = () => {
+    filterProduct("all");
+  };
